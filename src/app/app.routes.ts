8098065d7c3d6b1d45f918bfa3927922066bf516
Charlie Ermeti <Carlosermeti@hotmail.com>
2024-11-19 10:42:05 -0300

    Agegado de pantallas + estilos dash + footer feo + enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { LoginComponent } from './componentes/login/login.component';
 import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { authGuard } from './authGuard';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { ProveedoresComponent } from './componentes/proveedores/proveedores.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { ComprasComponent } from './componentes/compras/compras.component';
+import { VentasComponent } from './componentes/ventas/ventas.component';
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
 
 export const routes: Routes = [ { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
@@ -13,7 +19,12 @@ export const routes: Routes = [ { path: '', redirectTo: 'login', pathMatch: 'ful
       canActivate: [authGuard], // Usa el authGuard aquí
       children: [
         { path: 'inicio', component: InicioComponent },
-        
+        { path: 'usuarios', component: UsuariosComponent },
+        { path: 'clientes', component: ClientesComponent },
+        { path: 'proveedores', component: ProveedoresComponent },        
+        { path: 'productos', component: ProductosComponent },
+        { path: 'compras', component: ComprasComponent },
+        { path: 'ventas', component: VentasComponent },
       ],
     },
   ];
---
+++ src/app/componentes/genericos/popup-eliminar/popup-eliminar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-eliminar',
+  standalone: true,
+  imports: [],
+  templateUrl: './popup-eliminar.component.html',
+  styleUrl: './popup-eliminar.component.css'
+})
+export class PopupEliminarComponent {
+
+}
